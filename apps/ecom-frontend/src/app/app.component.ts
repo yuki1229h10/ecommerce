9678e19d6be378edf9d310d8e2a11a5675abf6a0
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-11-04 00:37:21 +0900

    update

--- apps/ecom-frontend/src/app/app.component.ts
+++ apps/ecom-frontend/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecom-frontend';
+export class AppComponent implements OnInit{
+
+private faIconLibrary = inject(FaIconLibrary);
+
+ngOnInit(): void {
+    this.initFontAwesome();
+}
+
+private initFontAwesome() {
+  this.faIconLibrary
+}
+
 }
